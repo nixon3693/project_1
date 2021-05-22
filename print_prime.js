dd69ba3a0015fb712de1e673df450195eeace22d
nixon3693 <84618011+nixon3693@users.noreply.github.com>
2021-05-22 14:40:15 +0530

    Add files via upload

---
+++ print_prime.js
@@ -0,0 +1,27 @@
+function prime(){
+// postMessage(localStorage.getItem("number"));
+postMessage("yes");
+}
+
+
+function prime(number){
+  if(number>1){
+    for (let i =0; i<number; i++){
+      flag =0;
+      for (let j=2; j<i; j++){
+        if(i%j == 0){
+          flag = 1;
+          break;
+        }
+      }
+      if (i>1 && flag == 0){
+        console.log(i,)
+      }
+    }
+  }
+  else{
+    console.log("not prime")
+  }
+}
+
+prime(10);
